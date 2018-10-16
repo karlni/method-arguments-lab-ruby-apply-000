66786c9a0008c9750a047adc4e72bb200f61feb4
agent.karl.zl.ni@gmail.com <agent.karl.zl.ni@gmail.com>
2018-10-16 15:48:47 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,6 @@ def introduction (Josh)
   puts "Hi, my name is #{Josh}."
 end
 
+def introduction_with_language (name, language)
+  puts "Hi,  my name is #{name} and I learning to program in #{language}."
+end
