6bdc925a157cc96e3c0c277069f031a5d49281e7
agent.karl.zl.ni@gmail.com <agent.karl.zl.ni@gmail.com>
2018-10-16 15:53:48 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,3 +5,10 @@ end
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+
+echo "# lib-introduction.rb" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/karlni/lib-introduction.rb.git
+git push -u origin master
